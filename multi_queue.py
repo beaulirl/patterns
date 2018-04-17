356d0a3eb93957cf5c99bd4bbf5cbd1c78a7f8c4
Elena Reutova <Elena.Reutova@artezio.com>
2018-04-17 12:30:11 +0300

    Add lock in counter

--- multi_queue.py
+++ multi_queue.py
@@ -5,11 +5,29 @@ import urllib.request
 from queue import Queue
 
 
+class Counter:
+    def __init__(self, start=0):
+        self.lock = threading.Lock()
+        self.value = start
+
+    def increment(self):
+        print('Waiting for a lock')
+        self.lock.acquire()
+        try:
+            print('Acquired a lock')
+            self.value += 1
+            print(self.value)
+        finally:
+            print('Released a lock')
+            self.lock.release()
+
+
 class Downloader(threading.Thread):
 
-    def __init__(self, queue):
+    def __init__(self, queue, counter):
         threading.Thread.__init__(self)
         self.queue = queue
+        self.v = counter
 
     def run(self):
         while True:
@@ -19,6 +37,8 @@ class Downloader(threading.Thread):
 
     def download_file(self, url):
         handle = urllib.request.urlopen(url)
+        print(threading.current_thread())
+        self.v.increment()
         fname = os.path.basename(url)
 
         with open(fname, 'wb') as f:
@@ -30,10 +50,11 @@ class Downloader(threading.Thread):
 
 
 def main(urls):
+    counter = Counter()
     queue = Queue()
 
     for i in range(5):
-        t = Downloader(queue)
+        t = Downloader(queue, counter)
         t.setDaemon(True)
         t.start()
 
